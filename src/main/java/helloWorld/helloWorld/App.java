c4fe3354728cef6baed4886e139103bdb84fa5b8
Rohan Singh <rohan.singh@capgemini.com>
2018-11-14 13:57:35 +0530

    fifth commit

--- src/main/java/helloWorld/helloWorld/App.java
+++ src/main/java/helloWorld/helloWorld/App.java
@@ -6,6 +6,7 @@ package helloWorld.helloWorld;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hi this is the start of the program!");
+		System.out.println("hello from rohan");
 	}
 }
